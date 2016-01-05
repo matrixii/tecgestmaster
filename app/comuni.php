5835a6da7dbf41248dde912970dcc9006aeb280b
mpica <marianopica@fastwebnet.it>
2016-01-05 18:04:57 +0100

    inizio metodi registrazione utente e log

---
+++ app/comuni.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\DB;
+
+class Comuni extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $table = 'comuni';
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+
+}
